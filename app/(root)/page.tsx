5e01d90b41c9376051ed4958a9668190f8cf8fa3
Brightosman <brichiile@outlook.com>
2025-02-27 18:16:31 +0100

    1st commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,18 @@
+import ProductList from "@/components/shared/product/product-list";
+import { Button } from "@/components/ui/button";
+// import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
+import Image from "next/image";
+
+// export const metadata = { title: "Home"}
+
+export default async function Homepage() {
+  const latestProducts = await getLatestProducts();
+  console.log("latestProducts", latestProducts);
+  return (
+    <div>
+        <Button>ProStore</Button> 
+        <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
+    </div>
+  );
+}
